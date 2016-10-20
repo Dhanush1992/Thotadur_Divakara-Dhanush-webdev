c8daad46310d58917971581d7a0e3d44c16c4087
Dhanush1992 <dhanushdiwakar@gmail.com>
2016-10-19 20:37:00 -0400

    Grouping of pages, initialized webappmaker in app.js and config.js

---
+++ public/assignment/config.js
@@ -0,0 +1,14 @@
+(function() {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "/views/user/login.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "/views/user/register.view.client.html"
+            })
+    }
+})();
